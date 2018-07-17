1939f7129cdbe592637ee716bc6dfe9f6cd1de4d
usmankhi18 <usman.khi18@hotmail.com>
2018-07-17 13:23:41 +0500

    Student Structure

---
+++ app/src/main/java/io/github/usmankhi18/attendancetrackingsystem/Student/UpdateStudentActivity.java
@@ -0,0 +1,13 @@
+package io.github.usmankhi18.attendancetrackingsystem.Student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_student);
+    }
+}
