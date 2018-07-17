374783ae533114e6375314679649b307f585a9f6
usmankhi18 <usman.khi18@hotmail.com>
2018-07-17 13:19:51 +0500

    Teacher Structure

---
+++ app/src/main/java/io/github/usmankhi18/attendancetrackingsystem/Teacher/UpdateTeacherActivity.java
@@ -0,0 +1,13 @@
+package io.github.usmankhi18.attendancetrackingsystem.Teacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateTeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_teacher);
+    }
+}
