cb94f7e59c5f31b4a4acbad5a1ad78e5a090b95a
usmankhi18 <usman.khi18@hotmail.com>
2018-07-17 13:31:24 +0500

    Mapping Structure

---
+++ app/src/main/java/io/github/usmankhi18/attendancetrackingsystem/Mapping/TeacherCourseMappingActivity.java
@@ -0,0 +1,13 @@
+package io.github.usmankhi18.attendancetrackingsystem.Mapping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherCourseMappingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_course_mapping);
+    }
+}
